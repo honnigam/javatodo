4233a3c09e75ac11da736ed5c4c206380e7dbe75
honnigam <lucasvm87@gmail.com>
2023-10-16 20:42:25 -0300

    increase:user.request

---
+++ src/main/java/br/com/honnigam/todojava/user/UserController.java
@@ -0,0 +1,37 @@
+package br.com.honnigam.todojava.user;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/* 
+ * Modificador
+    * public 
+    * private
+    * protected
+ */
+
+@RestController
+@RequestMapping ("/users")
+public class UserController {
+  
+  //escopo de acesso
+   /*
+    * String
+    * Integer (int) numeros inteiros
+    * Double (double) numeros 0.00000
+    * Float (float) numeros 0.000
+    * char (A C D) - caracteres
+    * Date (data) 
+    * void - não tem retorno, só executa a lógica
+   */ 
+
+   /*
+    * A REQ vem direto do body
+    */
+  @PostMapping("/")
+  public void create(@RequestBody UserModel userModel) {
+      System.out.println(userModel.name);
+  }
+}
---
+++ src/main/java/br/com/honnigam/todojava/user/UserModel.java
@@ -0,0 +1,7 @@
+package br.com.honnigam.todojava.user;
+
+public class UserModel {
+  public String name;
+  public String username;
+  public String password;
+}
