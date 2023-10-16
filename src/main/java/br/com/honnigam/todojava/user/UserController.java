4bcd034de8c7b35a19ac63cfbf4c8e5b197f63cc
honnigam <lucasvm87@gmail.com>
2023-10-16 20:54:55 -0300

    to add:lib:lombok checkpoint structure getters and setters

--- src/main/java/br/com/honnigam/todojava/user/UserController.java
+++ src/main/java/br/com/honnigam/todojava/user/UserController.java
@@ -32,6 +32,6 @@ public class UserController {
     */
   @PostMapping("/")
   public void create(@RequestBody UserModel userModel) {
-      System.out.println(userModel.name);
+      System.out.println(userModel.getPassword());
   }
 }
--- src/main/java/br/com/honnigam/todojava/user/UserModel.java
+++ src/main/java/br/com/honnigam/todojava/user/UserModel.java
@@ -1,7 +1,37 @@
 package br.com.honnigam.todojava.user;
 
+
 public class UserModel {
-  public String name;
-  public String username;
-  public String password;
+  private String name;
+  private String username;
+  private String password;
+  
+
+  // two types atributes => (getters / setters)
+  //inserir o valor
+  public void setName(String name) {
+    this.name = name;
+  }
+    //buscar o valor
+  public String getName() {
+    return name;
+  }
+    //inserir o valor
+  public void setUsername(String username) {
+    this.username = username;
+  }
+    //buscar o valor
+  public String getUsername() {
+    return username;
+  }
+    //inserir o valor
+  public void setPassword(String password) {
+    this.password = password;
+  }
+    //buscar o valor
+  public String getPassword() {
+    return password;
+  }
+
+
 }
