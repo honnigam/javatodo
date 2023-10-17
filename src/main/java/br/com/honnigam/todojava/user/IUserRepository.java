2e725d6712fda4ec27074d9281ea5b2acb90e89d
honnigam <lucasvm87@gmail.com>
2023-10-16 23:11:00 -0300

    create: creation of already created user

--- src/main/java/br/com/honnigam/todojava/user/IUserRepository.java
+++ src/main/java/br/com/honnigam/todojava/user/IUserRepository.java
@@ -4,7 +4,8 @@ import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 //model of application
 public interface IUserRepository extends JpaRepository<UserModel, UUID> {
-  
+  UserModel findByUsername(String username);
 }
--- src/main/java/br/com/honnigam/todojava/user/UserController.java
+++ src/main/java/br/com/honnigam/todojava/user/UserController.java
@@ -1,6 +1,9 @@
 package br.com.honnigam.todojava.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -34,8 +37,16 @@ public class UserController {
     * A REQ vem direto do body
     */
   @PostMapping("/")
-  public UserModel create(@RequestBody UserModel userModel) {
+  public ResponseEntity create(@RequestBody UserModel userModel) {
+      var user = this.userRepository.findByUsername(userModel.getUsername());
+      
+      if (user != null) {
+        //Error menssage
+        //Status code
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe!");
+      }
+
       var userCreated = this.userRepository.save(userModel);
-      return userCreated; 
+      return ResponseEntity.status(HttpStatus.CREATED).body(userCreated); 
   }
 }
